4d9ef87638433423afd38c23a54aface0d71a84e
Federico-xaca <fede.preno@gmail.com>
2023-07-24 14:46:41 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { shirt } from 'src/model/modes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'ngPractico';
+  showTable: boolean= true
+  showList: boolean= true
+
+  shirts: shirt[]= [
+    {
+      id:1,
+      name:"Messi",
+      price:150                     
+    },
+    {
+      id:2,
+      name:"Dibu",
+      price:130                     
+    },
+    {
+      id:3,
+      name:"DiMaria",
+      price:124                     
+    },
+    {
+      id:4,
+      name:"Paredes",
+      price:80                     
+    },
+
+  ]
+  displayedColumns: string[] = ['name', 'price'];
+
+  toggleTable() {
+    this.showTable = !this.showTable;
+  }
+
+  toggleList() {
+    this.showList = !this.showList;
+  }
 }
---
+++ src/model/modes.ts
@@ -0,0 +1,6 @@
+export type shirt= {
+    id:number,
+    name: string,
+    price: number
+  }
+  
